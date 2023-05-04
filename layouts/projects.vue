4490c71892c906d2b2201013df7dc26984146d14
Thibaut Colnot <112936057+ThibClnt@users.noreply.github.com>
2023-05-04 23:06:33 +0200

    Front champs simples création projet (#10 / #20)
    #10 Début page de création des projets :
    Nom, couleur, description, catégories

---
+++ layouts/projects.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div class="page-wrapper">
+    
+    <div class="top-line"></div>
+
+    <div class="body">
+
+      <slot />
+
+    </div>
+
+    <Footer></Footer>
+
+  </div>
+
+</template>
+
+
+<script lang="ts" setup></script>
+
+
+<style lang="scss" scoped>
+
+.page-wrapper {
+  position: relative;
+  min-height: 100vh;
+}
+
+.top-line {
+  background: #000;
+  height: 5px;
+}
+
+.body {
+  margin: 5px 20px;
+  padding-bottom: 2.5rem;
+}
+
+</style>
