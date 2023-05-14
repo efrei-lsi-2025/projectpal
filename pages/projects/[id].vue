c4ee18d73385ad885c9872ccaea86f9ba5eeeed7
JiveOff <antoine@jiveoff.fr>
2023-05-14 22:39:33 +0200

    Project page beginning + random fixes with Middleware

---
+++ pages/projects/[id].vue
@@ -0,0 +1,10 @@
+<template>
+  {{ project }}
+</template>
+
+<script lang="ts" setup>
+const auth = useAuth();
+const route = useRoute();
+
+const project = await useGetProjectById(route.params.id as number);
+</script>
