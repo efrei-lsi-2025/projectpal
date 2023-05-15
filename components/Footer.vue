04339a9c96089f96ae0067214c3816cb5fbcc91c
JiveOff <antoine@jiveoff.fr>
2023-05-15 23:14:14 +0200

    Branding

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="footer">
-    Build {{ shortSha }} ({{ config.public.vercel.branch }} -
-    {{ config.public.vercel.env }})
+    <span
+      >Build {{ shortSha }} ({{ config.public.vercel.branch }} -
+      {{ config.public.vercel.env }})</span
+    >
+    <span>
+      Built with <img src="../assets/branding/nuxt.svg" />
+      <a href="https://nuxtjs.org">Nuxt.js</a> and
+      <img src="../assets/branding/primevue.svg" />
+      <a href="https://primefaces.org/primevue/">PrimeVue</a>
+    </span>
   </div>
 </template>
 
@@ -20,7 +28,22 @@ const shortSha = config.public.vercel.commitSha.slice(0, 7);
   background-color: #000;
   color: #fff;
   display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   padding: 0.5rem 1rem;
+
+  a {
+    color: #fff;
+    font-weight: bold;
+    text-decoration: none;
+  }
+
+  img {
+    height: 1rem;
+    margin-right: 0.2rem;
+    margin-left: 0.1rem;
+    transform: translateY(0.15rem);
+  }
 }
 </style>
