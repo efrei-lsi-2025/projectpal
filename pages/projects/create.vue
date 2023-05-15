d0e47116ed9c92f06cae1b09f58d05d75817f3c8
JiveOff <antoine@jiveoff.fr>
2023-05-15 22:47:02 +0200

    Resolve conflicts

--- pages/projects/create.vue
+++ pages/projects/create.vue
@@ -1,37 +1,52 @@
 <template>
-    <div class="project-layout">
-        <div class="py-3 align-items-center formgrid grid">
-            <div class="field col-12 md:col-6 pr-5">
-                <h2 class="mr-4">Nom du projet</h2>
-                <InputText id="name" class=" w-10" :class="{ 'p-invalid': errorMessage }" v-model="name" />
-                <small class="p-error" id="text-error">{{ errorMessage || '&nbsp;' }}</small>
-            </div>
+  <div class="project-layout">
+    <div class="py-3 align-items-center formgrid grid">
+      <div class="field col-12 md:col-6 pr-5">
+        <h2 class="mr-4">Nom du projet</h2>
+        <InputText
+          id="name"
+          class="w-10"
+          :class="{ 'p-invalid': errorMessage }"
+          v-model="name"
+        />
+        <small class="p-error" id="text-error">{{
+          errorMessage || "&nbsp;"
+        }}</small>
+      </div>
 
-            <div class="field col-12 md:col-6">
-                <h2 class="mr-5">Client</h2>
-                <ClientLookup class="mr-6 w-10" @client-selected="setSelectedClient" :client-list="clientList" />
-                <ColorPicker v-model="color" />
-            </div>
-        </div>
+      <div class="field col-12 md:col-6">
+        <h2 class="mr-5">Client</h2>
+        <ClientLookup
+          class="mr-6 w-10"
+          @client-selected="setSelectedClient"
+          :client-list="clientList"
+        />
+        <ColorPicker v-model="color" />
+      </div>
+    </div>
 
-        <div class="py-3">
-            <h2>Description</h2>
-            <TextArea v-model="description" autoResize rows="5" class="w-full" />
-        </div>
+    <div class="py-3">
+      <h2>Description</h2>
+      <TextArea v-model="description" autoResize rows="5" class="w-full" />
+    </div>
 
-        <div class="py-3">
-            <h2>Utilisateurs</h2>
-            <ProjectsUserTable :model-value="members" :users-available="userList" />
-        </div>
+    <div class="py-3">
+      <h2>Utilisateurs</h2>
+      <ProjectsUserTable :model-value="members" :users-available="userList" />
+    </div>
 
-        <div class="py-3">
-            <h2>Catégories</h2>
-            <Chips v-model="categories" class="block" />
-        </div>
+    <div class="py-3">
+      <h2>Catégories</h2>
+      <Chips v-model="categories" class="block" />
+    </div>
 
-        <div class="py-3">
-            <Button icon="pi pi-check" label="Valider" severity="success" @click="createNewProject" />
-        </div>
+    <div class="py-3">
+      <Button
+        icon="pi pi-check"
+        label="Valider"
+        severity="success"
+        @click="createNewProject"
+      />
     </div>
 
     <div class="py-3">
@@ -75,11 +90,13 @@ const color = ref("bebebe");
 const categories: Ref<Array<string>> = ref([]);
 
 const clientList = computed(() => {
-    return allClients.value?.map(client => client.name)
-})
+  return allClients.value?.map((client) => client.name);
+});
 
 const selectedClient = ref("");
-const setSelectedClient = (client: string) => { selectedClient.value = client; }
+const setSelectedClient = (client: string) => {
+  selectedClient.value = client;
+};
 
 const userList = ref(allUsers);
 const members: Ref<Array<any>> = ref([]); // TODO: type with project members endpoint
@@ -96,57 +113,55 @@ if (user) {
 }
 
 // Form validation
-const errorMessage = ref("")
+const errorMessage = ref("");
 
 function validateForm() {
-    if (!name.value) {
-        errorMessage.value = 'Un nom de projet est requis.';
-    } else {
-        errorMessage.value = ""
-        return true;
-    }
+  if (!name.value) {
+    errorMessage.value = "Un nom de projet est requis.";
+  } else {
+    errorMessage.value = "";
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 // Create project on submit
 const createNewProject = async () => {
+  validateForm();
+  if (!name.value) return;
 
-    validateForm();
-    if (!name.value) return;
+  if (clientList.value?.includes(selectedClient.value)) {
+    // Dialog, selon retour on quitte
+  }
 
-    if (clientList.value?.includes(selectedClient.value)) {
-        // Dialog, selon retour on quitte
-
-    }
-
-    // Give order to the ticket states and add in a list
-    const ticketStates: Array<{ name: string, order: number }> = [];
-    let i = 0;
-    categories.value.forEach(category => {
-        ticketStates.push({
-            name: category,
-            order: i++,
-        });
+  // Give order to the ticket states and add in a list
+  const ticketStates: Array<{ name: string; order: number }> = [];
+  let i = 0;
+  categories.value.forEach((category) => {
+    ticketStates.push({
+      name: category,
+      order: i++,
     });
+  });
 
-    // Create list of members with userId and role
-    const projectMembers = members.value.map(member => {
-        return {
-            userId: member.userId,
-            role: member.role
-        };
-    });
+  // Create list of members with userId and role
+  const projectMembers = members.value.map((member) => {
+    return {
+      userId: member.userId,
+      role: member.role,
+    };
+  });
 
-    // Create the project
-    await createProject({
-        name: name.value,
-        description: description.value,
-        color: color.value,
-        client: selectedClient.value,
-        ticketStates: ticketStates,
-        projectMembers: projectMembers
-    });
+  // Create the project
+  await createProject({
+    name: name.value,
+    description: description.value,
+    color: color.value,
+    client: selectedClient.value,
+    ticketStates: ticketStates,
+    projectMembers: projectMembers,
+  });
 };
 </script>
 
